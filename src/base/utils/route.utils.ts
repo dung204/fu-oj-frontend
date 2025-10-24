5ee905f74b8975b883a56753e683361c0542a22d
dung204 <acezombiev4@gmail.com>
2025-10-24 23:04:38 +0700

    fix: update private route patterns to use correct syntax

--- src/base/utils/route.utils.ts
+++ src/base/utils/route.utils.ts
@@ -1,13 +1,13 @@
 import { pathToRegexp } from 'path-to-regexp';
 
 export const privateRoutes = [
-  '/user/*path',
-  '/exercises/*path',
-  '/submissions/*path',
-  '/leaderboard/*path',
+  '/user{/*path}',
+  '/exercises{/*path}',
+  '/submissions/{*path}',
+  '/leaderboard{/*path}',
   '/profile',
-  '/admin/*path',
-  '/groups/*path',
+  '/admin{/*path}',
+  '/groups{/*path}',
 ];
 
 export function checkIsPrivateRoute(route: string) {
